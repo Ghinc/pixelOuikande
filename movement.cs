075e8efc21188cd4f6b7bef2477eb501f8b59e99
Ghinevra <71076162+Ghinc@users.noreply.github.com>
2021-01-22 21:45:41 +0100

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    CharacterController controller;
+    public float speed ;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(Time.deltaTime*speed*move);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+}
